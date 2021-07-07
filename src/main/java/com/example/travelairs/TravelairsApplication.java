3b4cfe77776c0ff26f3d8fc6b2b2f887b671453b
jaemee <55077337+jaemee@users.noreply.github.com>
2021-07-07 20:31:56 +0800

    Initial commit

---
+++ src/main/java/com/example/travelairs/TravelairsApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelairs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelairsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelairsApplication.class, args);
+	}
+
+}
